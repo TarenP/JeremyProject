465497730c3c3ab97f15b35adf2574e3ed614601
Olivli <olivli148@gmail.com>
2023-04-18 14:01:19 -0700

    Update jerenmy.py

--- jerenmy.py
+++ jerenmy.py
@@ -50,33 +50,69 @@ inThrow = False
 inMotion = False
 num = 0
 
+def reset():
+    global person
+    global inThrow
+    global inMotion
+    global time
+    global num
+    global paper_x
+    global paper_y
+    global init_v
+    global angle
+    global time_increment
+    global acceleration
+    global p1X
+    global p1Y
+    global p2X
+    global p2Y
+    global p1fX
+    global p1fY
+    global p2fX
+    global p2fY
+    global touched
+    global d1
+    global d2
+
+    paper_x = 10
+    paper_y = Y_Size * .35
+    person = pygame.image.load("Images/BackThrow.png")
+    person = pygame.transform.scale(person, (300, 400))
+    inThrow = False
+        
+    inMotion = False
+    time = 1
+    num = 0
+
+    init_v = 20
+    angle = 2
+    #init_vx = math.cos(angle)*init_v
+    #init_vy = math.sin(angle)*init_v
+
+    time_increment = 1
+    time = 1
+    acceleration = 1
+    p1X = 70
+    p1Y = 370
+    p2X = 150
+    p2Y = 370
+    touched = False
+    p1fX = 70
+    p1fY = 370
+    p2fX = 150
+    p2fY = 370
+
+    d1 = math.sqrt((p1X-paper_x)**2 + (p1Y-paper_y)**2)
+    d2 = math.sqrt((p2X-paper_x)**2 + (p2Y-paper_y)**2)
 
 while True:
     if paper_x > 1000:
-
-        paper_x = 10
-        paper_y = Y_Size * .35
-
-        # screen.blit(paper, (10 , Y_Size*.35))
-        person = pygame.image.load("Images/BackThrow.png")
-        person = pygame.transform.scale(person, (300, 400))
-        inThrow = False
-        inMotion = False
-        time = 1
-        num = 0
+        reset()
+        
 
     elif paper_y > 1000:
-
-        paper_x = 10
-        paper_y = Y_Size * .35
-
-        # screen.blit(paper, (10 , Y_Size*.35))
-        person = pygame.image.load("Images/BackThrow.png")
-        person = pygame.transform.scale(person, (300, 400))
-        inThrow = False
-        inMotion = False
-        time = 1
-        num = 0
+        reset()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -115,37 +151,10 @@ while True:
                 p2fX = d2*math.cos(math.radians(angle))
                 p2fY = d2*math.sin(math.radians(angle))
             elif event.key == pygame.K_r:
+                reset()
 
                 #create variable to store the papers position
-                paper_x = 10
-                paper_y = Y_Size * .35
-                init_v = 20
-                angle = 2
-                #init_vx = math.cos(angle)*init_v
-                #init_vy = math.sin(angle)*init_v
-
-                time_increment = 1
-                time = 1
-                acceleration = 1
-                p1X = 70
-                p1Y = 370
-                p2X = 150
-                p2Y = 370
-                touched = False
-                p1fX = 70
-                p1fY = 370
-                p2fX = 150g
-                p2fY = 370
-
-                d1 = math.sqrt((p1X-paper_x)**2 + (p1Y-paper_y)**2)
-                d2 = math.sqrt((p2X-paper_x)**2 + (p2Y-paper_y)**2)
-
-                # screen.blit(paper, (10 , Y_Size*.35))
-                person = pygame.image.load("Images/BackThrow.png")
-                person = pygame.transform.scale(person, (300, 400))
-                inThrow = False
-                inMotion = False
-                num = 0
+                
 
 
     if inThrow:
